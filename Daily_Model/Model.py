57d1f727a1e684fcf0d0881a1dd4bc73ad6261e2
luisabreu102030 <36260657+luisabreu102030@users.noreply.github.com>
2021-04-21 17:57:59 +0100

    Join datasets
    Join dos datasets diários Diabetes e Ebola concluída.
    
    Adicionado um link em Time_series_forecasting_models.txt que ajuda a preparar um dataset para forecasting

--- Daily_Model/Model.py
+++ Daily_Model/Model.py
@@ -1,6 +1,16 @@
-import numpy as np
 import pandas as pd
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import TimeSeriesSplit
+import matplotlib.pyplot as plt
 
+
+#for replicability purposes
+tf.random.set_seed(91195003)
+np.random.seed(91190530)
+#for an easy reset backend session state
+tf.keras.backend.clear_session()
 
 
 if __name__ == '__main__':
--- Daily_Model/Process_Data.py
+++ Daily_Model/Process_Data.py
@@ -1,16 +1,85 @@
 import numpy as np
 import pandas as pd
 
+#Load dataset
+def load_datasets(path, path1):
+    return pd.read_csv(path, sep=';'), pd.read_csv(path1, sep=';')
+
+def prepare_data_diabetes(df_raw):
+    df_aux = df_raw.drop(columns=['All causes (A00-R99)'], inplace=False)
+
+    df_aux = df_aux
+    df_aux.columns = ['Date', 'Diabetes']
 
 
-def processData():
-    d = {'col1': [1, 2, 3, 4, 5, 6, 7, 8, 9], 'col2': [10, 11, 12, 13, 14, 15, 16, 17, 18]}
-    dataset = pd.DataFrame(data=d)
+    # alterar tipo da coluna Diabetes para numerico :
+    # Alterar tipo da coluna Date para datetime
+    df_aux["Diabetes"] = pd.to_numeric(df_aux["Diabetes"])
+    df_aux["Date"] = pd.to_datetime(df_aux["Date"])
+
+    #somar mortes no mesmo dia
+    df_aux = df_aux.groupby(['Date']).sum()
+
+    pd.set_option('display.max_rows', None)
+    #print(df_aux)
+    return df_aux
 
 
-    print(dataset.head(5))
+def prepare_data_ebola(df_raw_ebola):
+    print(df_raw_ebola.columns)
+    df_aux = df_raw_ebola.drop(columns=['Country', 'Localite', 'Sources', 'Link'], inplace=False)
+    
+    #Selecionar só registos de mortes
+    are_deaths_cases = df_aux['Category'] == 'Deaths'
+    df_aux = df_aux[are_deaths_cases]
+    #Drop da coluna Category
+    df_aux = df_aux.drop(columns=['Category'], inplace=False)
+    df_aux.columns = ['Ebola', 'Date']
+
+
+    #remover espaços da coluna Ebola
+    df_aux["Ebola"] = df_aux['Ebola'].str.replace(' ', '')
+
+    #alterar tipo da coluna Ebola para numerico :
+    #Alterar tipo da coluna Date para datetime
+    df_aux["Ebola"] = pd.to_numeric(df_aux["Ebola"])
+    df_aux["Date"] = pd.to_datetime(df_aux["Date"])
+
+    #somar mortes no mesmo dia
+    df_aux = df_aux.groupby(['Date']).sum()
+
+    #ordenar data
+    #df_aux['Date'] = pd.to_datetime(df_aux['Date'])
+    #df_aux = df_aux.sort_values(by='Date')
+
+
+    pd.set_option('display.max_rows', None)
+    #print(df_aux)
+    return df_aux
+
+
+def unite_dataframe(df_data_diabetes, df_data_ebola):
+    #Diabetes tem valores \to\do o ano, ebola já não ....
+    df_aux = pd.merge(df_data_diabetes, df_data_ebola, how="left", on=["Date", "Date"])
+
+    pd.set_option('display.max_rows', None)
+    print(df_aux)
+    return df_aux
+
 
 
 if __name__ == '__main__':
-    print("Hello world")
-    processData()
+    
+    #load datasets
+    df_raw_diabetes, df_raw_ebola = load_datasets("Diabetes_2014_England.csv", "daily_ebola_deaths_africa_2014.csv")
+    
+    #preparacao inicial de datasets
+    df_data_diabetes = prepare_data_diabetes(df_raw_diabetes)
+    df_data_ebola = prepare_data_ebola(df_raw_ebola)
+    
+    #join dos datasets
+    df_total = unite_dataframe(df_data_diabetes, df_data_ebola)
+
+    #DUVIDA: Como tratar os NaN. Que periodo de dias escolher ?
+    
+    
